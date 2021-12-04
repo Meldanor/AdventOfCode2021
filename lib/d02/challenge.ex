d955f460e76eb616d3ae7cd257dd1f67b0f1f7b4
meldanor <bhaals@gmx.de>
2021-12-04 17:13:24 +0100

    Solve day 2 second challenge

--- lib/d02/challenge.ex
+++ lib/d02/challenge.ex
@@ -6,7 +6,7 @@ defmodule D02.Challenge do
   def run(1) do
     result =
       Utils.read_input(2, &map_input/1)
-      |> execute_path
+      |> execute_path_day_1
 
     Logger.info("Final position: #{inspect(result)}. Result: #{Tuple.product(result)}")
   end
@@ -14,21 +14,26 @@ defmodule D02.Challenge do
   def run(2) do
     result =
       Utils.read_input(2, &map_input/1)
-      |> execute_path
+      |> execute_path_day_2
 
-    Logger.info("Final position: #{inspect(result)} increased")
+    Logger.info(
+      "Final position: #{inspect(result)}. Result: #{elem(result, 0) * elem(result, 1)}"
+    )
   end
 
-  defp execute_path(path) do
-    path
-    |> Enum.reduce({0, 0}, &execute/2)
-  end
+  defp execute_path_day_1(path), do: Enum.reduce(path, {0, 0}, &execute_day_1/2)
 
-  defp execute({"forward", x}, {horizontal, depth}), do: {horizontal + x, depth}
+  defp execute_day_1({"forward", x}, {horizontal, depth}), do: {horizontal + x, depth}
+  defp execute_day_1({"down", x}, {horizontal, depth}), do: {horizontal, depth + x}
+  defp execute_day_1({"up", x}, {horizontal, depth}), do: {horizontal, depth - x}
 
-  defp execute({"down", x}, {horizontal, depth}), do: {horizontal, depth + x}
+  defp execute_path_day_2(path), do: Enum.reduce(path, {0, 0, 0}, &execute_day_2/2)
 
-  defp execute({"up", x}, {horizontal, depth}), do: {horizontal, depth - x}
+  defp execute_day_2({"forward", x}, {horizontal, depth, aim}),
+    do: {horizontal + x, depth + x * aim, aim}
+
+  defp execute_day_2({"down", x}, {horizontal, depth, aim}), do: {horizontal, depth, aim + x}
+  defp execute_day_2({"up", x}, {horizontal, depth, aim}), do: {horizontal, depth, aim - x}
 
   defp map_input(line) do
     split = String.split(line, " ")

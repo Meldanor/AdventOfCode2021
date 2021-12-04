32500b955a7b4624a011a686b4bafb23ccf981ea
meldanor <bhaals@gmx.de>
2021-12-04 13:10:10 +0100

    Solve day 1 second challenge

--- lib/d01/challenge.ex
+++ lib/d01/challenge.ex
@@ -3,21 +3,50 @@ defmodule D01.Challenge do
 
   require Logger
 
-  def run do
-    Logger.info("Day 1 challenge")
+  def run(1) do
+    result =
+      File.read!("./lib/d01/input")
+      |> String.split("\n")
+      |> Enum.reject(fn s -> String.length(s) == 0 end)
+      |> Enum.map(&String.to_integer/1)
+      |> count_increases()
 
-    IO.inspect(Path.absname("."))
-    result = File.read!("./lib/d01/input")
-    |> String.split("\n")
-    |> Enum.reject(fn s -> String.length(s) == 0 end)
-    |> Enum.map(&String.to_integer/1)
-    |> Enum.reduce(%{increased: 0, last: Integer.pow(2, 31) - 1}, fn e, %{increased: increased, last: last} ->
-      diff = (e - last) |> sign |> Kernel.max(0)
-      %{increased: increased + diff, last: e}
-    end)
     Logger.info("#{result.increased}x increased")
   end
 
-  defp sign(integer) when integer >= 0, do: 1
-  defp sign(integer) when integer < 0, do: -1
+  def run(2) do
+    result =
+      File.read!("./lib/d01/input")
+      |> String.split("\n")
+      |> Enum.reject(fn s -> String.length(s) == 0 end)
+      |> Enum.map(&String.to_integer/1)
+      |> sum_windows()
+      |> count_increases()
+
+    Logger.info("3-Sums #{result.increased}x increased")
+  end
+
+  defp sum_windows(values) do
+    values
+    |> Enum.chunk_every(3, 1, :discard)
+    |> Enum.map(&Enum.sum/1)
+    |> Enum.reject(fn i -> i == 0 end)
+  end
+
+  defp count_increases(values) do
+    values
+    |> Enum.reduce(
+      %{increased: 0, last: Integer.pow(2, 31) - 1},
+      fn e,
+         %{
+           increased: increased,
+           last: last
+         } ->
+        %{increased: increased + increased_scalar(e, last), last: e}
+      end
+    )
+  end
+
+  defp increased_scalar(a, b) when a > b, do: 1
+  defp increased_scalar(_a, _b), do: 0
 end
--- lib/mix/tasks/aoc.ex
+++ lib/mix/tasks/aoc.ex
@@ -2,7 +2,7 @@ defmodule Mix.Tasks.Aoc do
   @moduledoc "Runs a challenge for a given day. `mix aoc <Day>`, starting with 1."
   @shortdoc "Echoes arguments"
 
-  @challenges [&D01.Challenge.run/0]
+  @challenges [&D01.Challenge.run/1]
 
   use Mix.Task
 
@@ -11,8 +11,9 @@ defmodule Mix.Tasks.Aoc do
   @impl Mix.Task
   def run(args) do
     day = Enum.at(args, 0) |> String.to_integer()
+    challenge_index = Enum.at(args, 1) |> String.to_integer()
     challenge = Enum.at(@challenges, day - 1)
-    Logger.info("Executing challenge of day #{day}")
-    challenge.()
+    Logger.info("Day #{day} Challenge #{challenge_index}")
+    challenge.(challenge_index)
   end
 end

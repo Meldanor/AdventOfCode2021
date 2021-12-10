c0e625865a0b717f73b712c846a2f8f0729ad82b
meldanor <bhaals@gmx.de>
2021-12-10 19:07:14 +0100

    Solve day 10 second challenge

--- lib/d10/challenge.ex
+++ lib/d10/challenge.ex
@@ -3,6 +3,13 @@ defmodule D10.Challenge do
 
   require Logger
 
+  @brackets %{
+    "(" => ")",
+    "{" => "}",
+    "[" => "]",
+    "<" => ">"
+  }
+
   def run(1) do
     lines = Utils.read_input(10, &String.codepoints/1)
 
@@ -10,7 +17,7 @@ defmodule D10.Challenge do
       lines
       |> Enum.map(&reduce_line/1)
       |> Enum.reject(&is_list/1)
-      |> Enum.map(&point/1)
+      |> Enum.map(&point(&1, :corrupt))
       |> Enum.sum()
 
     Logger.info("The total syntax error score is #{result}")
@@ -18,6 +25,16 @@ defmodule D10.Challenge do
 
   def run(2) do
     lines = Utils.read_input(10, &String.codepoints/1)
+
+    result =
+      lines
+      |> Stream.map(&reduce_line/1)
+      |> Stream.filter(&is_list/1)
+      |> Stream.map(&complete_line/1)
+      |> Enum.map(&calculate_auto_complete_score/1)
+      |> find_middle_score()
+
+    Logger.info("The middle score is #{result}")
   end
 
   defp reduce_line(line) do
@@ -41,29 +58,45 @@ defmodule D10.Challenge do
     end)
   end
 
+  defp complete_line(line) do
+    line
+    |> Enum.reduce([], fn symbol, list ->
+      closer = Map.get(@brackets, symbol)
+      list ++ [closer]
+    end)
+  end
+
+  defp calculate_auto_complete_score(closing_chars) do
+    closing_chars
+    |> Enum.reduce(0, fn char, sum ->
+      sum * 5 + point(char, :incomplete)
+    end)
+  end
+
+  defp find_middle_score(scores) do
+    size = length(scores)
+
+    scores
+    |> Enum.sort()
+    |> Enum.at((size / 2) |> trunc)
+  end
+
   defp pop([]), do: {nil, []}
   defp pop(stack), do: {hd(stack), tl(stack)}
-
-  defp open?(char) do
-    char in ~w(( { [ <)
-  end
 
   defp close?(char) do
     char in ~w(\) } ] >)
   end
 
-  @brackets %{
-    "(" => ")",
-    "{" => "}",
-    "[" => "]",
-    "<" => ">"
-  }
-
-  defp opening?(left, right), do: Map.get(@brackets, left) == right
   defp closing?(right, left), do: Map.get(@brackets, left) == right
 
-  defp point(")"), do: 3
-  defp point("]"), do: 57
-  defp point("}"), do: 1197
-  defp point(">"), do: 25137
+  defp point(")", :corrupt), do: 3
+  defp point("]", :corrupt), do: 57
+  defp point("}", :corrupt), do: 1197
+  defp point(">", :corrupt), do: 25137
+
+  defp point(")", :incomplete), do: 1
+  defp point("]", :incomplete), do: 2
+  defp point("}", :incomplete), do: 3
+  defp point(">", :incomplete), do: 4
 end
